3a767dbb2d396843e4293a10d5108b7fcf5a0196
MSingh20055 <manmeetsingh20005@gmail.com>
2026-05-08 23:56:03 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const DiscordIcon = ({ size = 20 }) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
